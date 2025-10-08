d8c7c86478eaa7ce13fc1a145f369e592a00cba7
jilani.vn03@gmail.com <jilani.vn03@gmaol.com>
2025-10-08 10:48:55 +0200

    Test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ const USERS_DATA = [
   { id: '4', nom: 'David Petit', email: 'david.petit@email.com' },
   { id: '5', nom: 'Émilie Rousseau', email: 'emilie.rousseau@email.com' },
 ];
-//J'aime le choco//
+//J'aime le choco
 
 
 export default function HomeScreen() {
